bd4b7ba98dc4bbc0943bbfdc2d14e1dcff291522
koh11235813 <koh11235813@gmail.com>
2024-07-18 03:32:01 +0900

    main.c

--- Numeric Analysis/NA05/main.c
+++ Numeric Analysis/NA05/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define DIM 3
 
+void solvearray(x)
+double x[DIM][DIM+1];
+{
+    int i, j, k;
+    double d;
+
+    for(i = 0; i < DIM; i++){
+
+    }
+    d = x[i][i];
+    for(k = i; k <=DIM; i++){
+        for (j = 0; j < DIM; j++){
+            if(i != j)  {
+                d = x[j][i];
+                for(k = i; k <= DIM; k++) {
+                    x[j][k] -= x[i][k]*d;
+                }
+            }
+        }
+    }
+}
 int main(void)
 {
 
