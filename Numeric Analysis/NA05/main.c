dd25eb618141955105a3cd2cd494c1e9b8b84f7d
koh11235813 <koh11235813@gmail.com>
2024-07-19 13:17:06 +0900

    editting main.c on NA05

--- Numeric Analysis/NA05/main.c
+++ Numeric Analysis/NA05/main.c
@@ -10,20 +10,40 @@ double x[DIM][DIM+1];
     double d;
 
     for(i = 0; i < DIM; i++){
-
-    }
-    d = x[i][i];
-    for(k = i; k <=DIM; i++){
-        for (j = 0; j < DIM; j++){
-            if(i != j)  {
-                d = x[j][i];
-                for(k = i; k <= DIM; k++) {
-                    x[j][k] -= x[i][k]*d;
+        if (x[i][i] == 0.0) {
+            //exchange row
+            for (j = i; j < DIM; j++) {
+                if (x[j][i] != 0.0) {
+                    printf("--- exchanging row_%d and row?%d\n", i, j);
+                    for(k = i; k<= DIM; k++) {
+                        d = x[i][k];
+                        x[i][k] = x[j][k];
+                        x[j][k] = d;
+                    }
+                    break;
+                }
+            }
+            if ( j == DIM) {
+                //no non-zero pivot is available
+                printf("There is no non-zero pivot. Aborting...\n");
+                exit(-1);
+            }
+        }
+        d = x[i][i];
+        for(k = i; k <=DIM; k++){
+            x[i][k] /= d;
+            for (j = 0; j < DIM; j++){
+                if(i != j)  {
+                    d = x[j][i];
+                    for(k = i; k <= DIM; k++) {
+                        x[j][k] -= x[i][k]*d;
+                    }
                 }
             }
         }
     }
 }
+
 int main(void)
 {
 
